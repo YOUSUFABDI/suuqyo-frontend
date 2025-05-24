0d791618a80b5c30f82b4034c9861100243b615b
unknown <yufis0844@gmail.com>
2025-05-24 17:59:06 +0300

    w-on product

--- src/layouts/nav-config-main.tsx
+++ src/layouts/nav-config-main.tsx
@@ -6,15 +6,15 @@ import { paths } from 'src/routes/paths';
 // ----------------------------------------------------------------------
 
 export const navData: NavMainProps['data'] = [
-  { title: 'Home', path: '/', icon: <Iconify width={22} icon="solar:home-2-bold-duotone" /> },
+  { title: 'Home', path: '/', icon: <Iconify width={22} icon="tabler:home" /> },
   {
     title: 'Products',
     path: paths.customer.product.root,
-    icon: <Iconify width={22} icon="maki:shop" />,
+    icon: <Iconify width={22} icon="fluent:box-20-filled" />,
   },
   {
     title: 'Shops',
     path: paths.customer.shop.root,
-    icon: <Iconify width={22} icon="maki:shop" />,
+    icon: <Iconify width={22} icon="material-symbols:storefront-rounded" />,
   },
 ];
