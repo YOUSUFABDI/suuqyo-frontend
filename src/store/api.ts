0d1b45919b94aa9f011881c8ad542e98bc23460b
YOUSUFABDI <yufis0844@gmail.com>
2025-06-27 14:29:04 +0300

    deploying the frontend

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,2 +1,3 @@
-export const API = `${window?.location.protocol}//${window?.location.hostname}:8080`;
-// export const API = 'http://localhost:8080';
+// export const API = `${window?.location.protocol}//${window?.location.hostname}:8080`;
+// export const API = 'http://localhost:3000';
+export const API = 'http://104.251.216.88:3000';
