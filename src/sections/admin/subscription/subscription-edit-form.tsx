9dc7077282fe069c686ffd7f56e4fa190ec40024
unknown <yufis0844@gmail.com>
2025-07-18 16:58:39 +0300

    expanded the subscription

--- src/sections/admin/subscription/subscription-edit-form.tsx
+++ src/sections/admin/subscription/subscription-edit-form.tsx
@@ -37,7 +37,7 @@ const SUBSCRIPTION_TERMS = [
 // ----------------------------------------------------------------------
 
 interface SubscriptionEditFormProps {
-  currentSubscription: SubscriptionResDT;
+  currentSubscription?: SubscriptionResDT | null;
 }
 
 export function SubscriptionEditForm({ currentSubscription }: SubscriptionEditFormProps) {
